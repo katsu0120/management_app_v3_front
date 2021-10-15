d11a220d7c2ec6a34e28783036ee169c88abed54
katsu0120 <hayakatsu364@gmail.com>
2021-10-15 18:52:48 +0900

    57/Set data binding for singup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -2,9 +2,15 @@
   <user-form-card>
     <template #user-form-card-content>
       <v-form v-model="isValid">
-        <user-form-name />
-        <user-form-email />
-        <user-form-password />
+        <user-form-name
+          :name.sync="params.user.name"
+        />
+        <user-form-email
+          :email.sync="params.user.email"
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+        />
         <v-btn
           :disabled="!isValid"
           block
@@ -14,6 +20,7 @@
           登録する
         </v-btn>
       </v-form>
+      {{ params }}
     </template>
   </user-form-card>
 </template>
@@ -23,7 +30,8 @@ export default {
   layout: 'before-login',
   data () {
     return {
-      isValid: false
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   }
 }
