6f5630317ec6e3bae37ad74864f0267a9690f082
katsu0120 <hayakatsu364@gmail.com>
2021-10-16 23:00:10 +0900

    63/Add user account settings page

---
+++ pages/account.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <nuxt-child />
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'logged-in',
+  // falseを返すページのアクセスを制限する
+  validate ({ route }) {
+    return route.name !== 'account'
+  }
+}
+</script>
