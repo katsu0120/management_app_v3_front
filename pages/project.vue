f515a30d98637eb1144391d609b26a1548823975
katsu0120 <hayakatsu364@gmail.com>
2021-10-17 19:34:41 +0900

    65/Add individual project pages

---
+++ pages/project.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <nuxt-child />
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'project',
+  // falseを返すページのアクセスを制限する
+  validate ({ route }) {
+    return route.name !== 'project'
+  }
+}
+</script>
