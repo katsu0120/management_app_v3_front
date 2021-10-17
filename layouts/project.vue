9578f95eb8cf6d475f3853578223fc9cc7525d3b
katsu0120 <hayakatsu364@gmail.com>
2021-10-18 01:01:43 +0900

    66/Add project navigation drawer

--- layouts/project.vue
+++ layouts/project.vue
@@ -1,13 +1,34 @@
 <template>
   <v-app>
-    <logged-in-app-bar />
+    <!-- toolbar -->
+    <logged-in-app-bar clipped-left>
+      <template
+        #navigation-toggle-button
+      >
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+        />
+      </template>
+    </logged-in-app-bar>
+
+    <!-- navigation drawer -->
+    <project-navigation-drawer
+      :drawer.sync="drawer"
+    />
+
+    <!-- main content -->
     <v-main>
-      project.vue
       <nuxt />
     </v-main>
   </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
 </script>
