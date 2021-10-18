a8d7783b17fe7f64cb85afe6a7941f6c7f8a3079
katsu0120 <hayakatsu364@gmail.com>
2021-10-18 20:56:11 +0900

    69/Add breadcrumbs to LoggedInAppBar.vue

--- components/LoggedIn/LoggedInAppBar.vue
+++ components/LoggedIn/LoggedInAppBar.vue
@@ -15,10 +15,19 @@
       <app-logo />
     </nuxt-link>
 
-    <app-title />
+    <!-- app title -->
+    <app-title
+      :class="{ 'hidden-mobile-and-down' : isNotHomePath }"
+    />
+
+    <!-- page title -->
+    <logged-in-app-bar-breadcrumbs
+      v-if="isNotHomePath"
+    />
 
     <v-spacer />
 
+    <!-- account menu -->
     <logged-in-app-bar-account-menu />
   </v-app-bar>
 </template>
@@ -34,6 +43,11 @@ export default {
   data ({ $store }) {
     return {
       homePath: $store.state.loggedIn.homePath
+    }
+  },
+  computed: {
+    isNotHomePath () {
+      return this.$route.name !== this.homePath.name
     }
   }
 }
