1e08181593b3f7963b9cff2e5b7ab5e0a7f2cd49
katsu0120 <hayakatsu364@gmail.com>
2022-01-30 23:27:56 +0900

    会員登録Gメール認証の雛形実装が完了したのでコミット

---
+++ pages/account/activations.vue
@@ -0,0 +1,41 @@
+<template>
+  <div />
+</template>
+<script>
+export default {
+  // ① URLのクエリからトークンを取得
+  async asyncData ({ route }) {
+    const token = await route.query.token
+    return { token }
+  },
+  async created () {
+    // ② トークンが存在する場合、トークンを検証する
+    if (this.token) { await this.verifyToken() }
+    console.log('この下がcreate')
+    console.log(this.token)
+    // ③ トークンがnullの場合、何もせずリダイレクトする
+    // this.$router.replace('/')
+  },
+  methods: {
+    async verifyToken () {
+      const headers = {
+        headers: { Authorization: `Bearer ${this.token}` }
+      }
+      // ④ リクエストヘッダーにトークンをつけて、認証コントローラーへ渡す
+      await this.$axios.$get('/api/v1/account_activations', headers)
+        .then(response => this.validToken(response))
+      // ⑤トークンが検証エラーを吐いた場合、「もう一度処理を行ってください」などのトースターを出力する
+        .catch(error => this.$my.invalidToken(error))
+    },
+    // ⑥ トークンの検証に成功した場合、ログインフラグをtrueとし、ログイン画面を表示する
+    validToken (response) {
+      console.log('この下がresponse')
+      console.log(response)
+      this.$auth.login(response)
+      alert('会員登録が完了しました')
+      // ルートリダイレクト
+      this.$router.push('/')
+    }
+  }
+}
+</script>
