fa236069df6dafc61a10ed5d065a3eededc4c1ac
katsu0120 <hayakatsu364@gmail.com>
2022-03-07 18:19:00 +0900

    edit

--- middleware/get-company-project-list.js
+++ middleware/get-company-project-list.js
@@ -1,5 +1,4 @@
 export default async ({ store, $axios, params }) => {
-  console.log('これがparams', params)
   await $axios.$get('/api/v1/company_projects', { params })
     .then(projects => store.dispatch('getCompanyProjectList', projects))
 }
